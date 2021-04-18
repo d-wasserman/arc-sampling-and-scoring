177123e3d47808b34d852aca28534744d0c95439
d-wasserman <david.wasserman.plan@gmail.com>
2021-04-18 15:16:13 -0700

    Update ProportionalAllocation.py
    Working proportional allocation draft.

--- Scripts/Experiments/ProportionalAllocation.py
+++ Scripts/Experiments/ProportionalAllocation.py
@@ -56,23 +56,37 @@ def proportional_allocation(sampling_features, base_features, out_feature_class,
         # Start Analysis
         temp_intersect = os.path.join("in_memory", "temp_intersect")
         san.arc_print("Calculating original areas...")
-        san.add_new_field(base_features, "Base_Area_SQMI", "DOUBLE")
-        arcpy.CalculateField_management(base_features, "Base_Area_SQMI", "!shape.area@SQUAREMILES!")
+        base_area_col = "Base_Area_SQMI"
+        inter_area_col = "Inter_Area_SQMI"
+        sampling_id = "Sampling_ID"
+        ratio_coverage = "Proportion"
+        san.add_new_field(base_features, base_area_col, "DOUBLE")
+        arcpy.CalculateField_management(base_features, base_area_col, "!shape.area@SQUAREMILES!")
+        san.add_new_field(sampling_features, "Sampling_ID", "LONG")
+        oid_s = arcpy.Describe(sampling_features).OIDFieldName
+        arcpy.CalculateField_management(sampling_features, sampling_id, "!{0}!".format(oid_s))
         san.arc_print("Conducting an intersection...", True)
         arcpy.Intersect_analysis([[sampling_features, 1], [base_features, 1]], temp_intersect)
-        san.add_new_field(temp_intersect, "Inter_Area_SQMI", "DOUBLE")
-        arcpy.CalculateField_management(temp_intersect, "Inter_Area_SQMI", "!shape.area@SQUAREMILES!")
+        san.add_new_field(temp_intersect, inter_area_col, "DOUBLE")
+        arcpy.CalculateField_management(temp_intersect, inter_area_col, "!shape.area@SQUAREMILES!")
         san.arc_print("Calculating proportional sums and/or averages...", True)
-        all_fields = ["Base_Area_SQMI", "Inter_Area_SQMI"] + sum_fields + mean_fields
+        all_fields = [sampling_id, inter_area_col, base_area_col] + sum_fields + mean_fields
         inter_df = san.arcgis_table_to_df(temp_intersect, all_fields)
-        inter_df["Proportion"] = inter_df["Inter_Area_SQMI"].fillna(0)/inter_df["Base_Area_SQMI"].fillna(1)
-        for sum_field in sum_fields:
-            new_field = "ProSUM_" + str(sum_field)
-            inter_df
-        samp_df = pd.DataFrame.spatial.from_featureclass(sampling_features)
+        inter_df[ratio_coverage] = inter_df[inter_area_col].fillna(0) / inter_df[base_area_col].fillna(1)
+        all_stats = sum_fields + mean_fields
+        for field in all_stats:
+            inter_df[field] = inter_df[field] * inter_df[ratio_coverage]  # Weight X Value
+        inter_groups = inter_df.groupby(sampling_id).sum()
+        for mean in mean_fields:
+            inter_groups[mean] = inter_groups[mean] / inter_groups[ratio_coverage]  # Divide by sum of weights for wm
         san.arc_print("Associating results to sampled SEDF...")
-        san.arc_print(inter_df.head())
+        samp_df = pd.DataFrame.spatial.from_featureclass(sampling_features)
+        samp_df = samp_df.merge(inter_groups, how="left", left_on=sampling_id, right_index=True,
+                                suffixes=("DELETE_X", "DELETE_Y"))
+        kept_cols = [i for i in samp_df.columns if "DELETE" not in i]
+        samp_df = samp_df[kept_cols].copy()
         san.arc_print("Exporting results...", True)
+        samp_df.spatial.to_featureclass(out_feature_class)
         san.arc_print("Script Completed Successfully.", True)
     except arcpy.ExecuteError:
         san.arc_print(arcpy.GetMessages(2))
@@ -93,5 +107,4 @@ if __name__ == '__main__':
     output_feature_class = arcpy.GetParameterAsText(2)
     sum_fields = arcpy.GetParameterAsText(3).split(";")
     mean_fields = arcpy.GetParameterAsText(4).split(";")
-
     proportional_allocation(target_feature_class, join_feature_class, output_feature_class, sum_fields, mean_fields)
